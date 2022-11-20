e024c4f5aa47b3935c061941e3561bec79582d12
iQuick <iquickpublic@gmail.com>
2022-11-20 19:45:16 +0100

    comando clear

---
+++ commands/clear.js
@@ -0,0 +1,44 @@
+const { SlashCommandBuilder, PermissionsBitField } = require("discord.js")
+
+module.exports = {
+	data: new SlashCommandBuilder()
+		.setName("clear")
+		.setDescription("pulisci la chat")
+		.addIntegerOption((option) =>
+			option
+				.setName('quantità')
+				.setDescription("la quantità di messaggi da rimuovere")
+				.setRequired(true),
+		),
+	async execute(interaction) {
+		const quantity = interaction.options.getInteger('quantità')
+		const channel = await interaction.channel
+
+		if (!interaction.member.permissions.has(PermissionsBitField.Flags.ManageMessages)) {
+			await interaction.reply({
+				embeds: [
+					{
+						title: `❌ Si è verificato un errore`,
+						description: `Non hai i permessi necessari`,
+						color: 15548997,
+					},
+				],
+				ephemeral: true,
+			})
+			return
+		}
+
+		channel.bulkDelete(quantity)
+
+		await interaction.reply(
+			{
+				embeds: [
+					{
+						title: `✅ Chat pulita`,
+						color: 5763719,
+					},
+				],
+			},
+		)
+	},
+}
