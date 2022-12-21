dbcdf21e514d2b0bd4e758e7b30fba5114b5be2c
PolpOnline <git@t0mmy.anonaddy.com>
2022-12-21 22:10:26 +0100

    fixed counting.js

--- events/counting.js
+++ events/counting.js
@@ -1,23 +1,20 @@
 const { client } = require('../index.js')
+const countingChannelID = '1053039207855177739' //production
 
-const countingChannelID = "1053039207855177739"
+//const countingChannelID = '1052681467706212502' //testing
 
-let count = 0
+let count = 1
 
 client.on('messageCreate', (message) => {
-
 	const { channel, content, member } = message
 
 	if (channel.id === countingChannelID) {
 		if (member.user.bot) return
 
-		count++
-
 		if (Number(content) !== count) {
-			
 			message.delete()
 
-			console.error(`[Counting channel] ${member} ha sbagliato a contare, il contatore era a ${count-1}`)
-		}
+			console.error(`[Counting channel] ${member.displayName} ha sbagliato a contare, il contatore era a ${count - 1}, offending message "${message.content}"`)
+		} else count++
 	}
 })
