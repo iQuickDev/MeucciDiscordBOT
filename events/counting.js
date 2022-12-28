27be74a221883d7e5f04f77951c27ba378b02906
PolpOnline <git@t0mmy.anonaddy.com>
2022-12-28 23:28:20 +0100

    fixed counting on server restart by retrieving the count from the last message sent in the channel

--- events/counting.js
+++ events/counting.js
@@ -1,9 +1,19 @@
 const { client } = require('../index.js')
-const countingChannelID = '1053039207855177739' //production
 
-//const countingChannelID = '1052681467706212502' //testing
+let countingChannelID = '1053039207855177739' //production
+//let countingChannelID = '1052681467706212502' //testing
 
-let count = 1
+// take the last message from the counting channel as the counter start
+let count
+client.on('ready', () => {
+	const channel = client.channels.cache.get(countingChannelID)
+
+	channel.messages.fetch({ limit: 1 }).then((messages) => {
+		const lastMessage = messages.last()
+
+		count = Number(lastMessage.content) + 1
+	})
+})
 
 client.on('messageCreate', (message) => {
 	const { channel, content, member } = message
